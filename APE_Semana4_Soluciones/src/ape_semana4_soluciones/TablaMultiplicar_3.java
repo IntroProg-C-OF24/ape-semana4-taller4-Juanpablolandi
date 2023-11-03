c99af287488e14eb555327b166b3d9033924507f
Juan Pablo <jplandi@utpl.edu.ec>
2023-11-03 14:20:44 -0500

    Tabla de multiplicar

--- APE_Semana4_Soluciones/src/ape_semana4_soluciones/TablaMultiplicar_3.java
+++ APE_Semana4_Soluciones/src/ape_semana4_soluciones/TablaMultiplicar_3.java
@@ -1,9 +1,15 @@
 package ape_semana4_soluciones;
 import java.util.Scanner;
-public class TablaMultiplicar_3 {
+public class tablaMultiplicar_3 {
     public static void main(String[] args) {
-        i
-        
+        int numIngresar;
+        Scanner teclado = new Scanner (System.in); //DECLARANDO E INICIALIZANDO
+        System.out.println("Ingrese un número entre el 2 y 6: ");
+        numIngresar = teclado.nextInt();
+        System.out.println("TABLA PRESENTADA");
+        for (int i = 1; i <= 10; i++) {
+        //número*i = (número*i)
+            System.out.println(numIngresar * i);
+        }    
     }
-    
 }
